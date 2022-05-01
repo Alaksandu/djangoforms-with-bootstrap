d889c5848db3ad2bf8d33b43e2752494cfeee2c8
Alaksandu <alexandre.nike10@hotmail.com>
2022-05-01 16:05:45 -0300

    projeto finalizado

---
+++ core/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .forms import ContatoForm, ProdutoModelForm
+from .models import Produto
+
+
+def index(request):
+    context = {
+        'produtos': Produto.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def contato(request):
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_email()
+
+            messages.success(request, 'E-Mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request, 'Erro ao enviar E-Mail')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+
+def produto(request):
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'A mensagem foi salva com sucesso')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto')
+        else:
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
+    else:
+        return redirect('index')
