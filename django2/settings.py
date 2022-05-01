cdc38d19bd1ab127856e5c083fff3af751991406
Alaksandu <alexandre.nike10@hotmail.com>
2022-05-01 16:22:08 -0300

    alterando middleware_classes

--- django2/settings.py
+++ django2/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
 
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
